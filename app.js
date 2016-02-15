ae00384b8f01248b03462fa1bb72498fd92eb418
unknown <k.yakimtsev@kyakimtsev-nb.vinnica.astoundcommerce.com>
2016-02-15 12:24:44 +0200

    second

---
+++ app.js
@@ -0,0 +1,34 @@
+window.onload=function(){
+
+    /**
+    * TextCounter
+    */
+    var TextCounter = {
+        maxChar: 140,
+        textArea: null,
+        textAreaTotal: null,
+        textAreaLeft: null,
+
+        init: function() {
+            this.textArea = document.querySelector('#js-message');
+            this.textAreaTotal = document.querySelector('#js-message-left-total');
+            this.textAreaLeft = document.querySelector('#js-message-left-symbols');
+            this.textArea.setAttribute("maxlength", this.maxChar);
+            this.events();
+        },
+
+        events: function() {
+            var that = this;
+            this.textArea.addEventListener('keyup', function(e){
+                e.preventDefault();
+
+                var countMessage = that.textArea.value.length;
+                that.textAreaTotal.innerHTML = countMessage;
+
+                that.textAreaLeft.innerHTML = that.maxChar - countMessage;
+            });
+        }
+    }
+
+    TextCounter.init();
+}
