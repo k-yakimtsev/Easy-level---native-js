57047600c74dcafc92f60bffd528d5b34af9efe0
kostikf12 <k.yakimtsev@kyakimtsev-nb.vinnica.astoundcommerce.com>
2016-02-17 18:11:42 +0200

    keypress first

--- app.js
+++ app.js
@@ -13,20 +13,34 @@ window.onload=function(){
             this.textArea = document.querySelector('#js-message');
             this.textAreaTotal = document.querySelector('#js-message-left-total');
             this.textAreaLeft = document.querySelector('#js-message-left-symbols');
-            this.textArea.setAttribute("maxlength", this.maxChar);
             this.events();
         },
 
         events: function() {
             var that = this;
-            this.textArea.addEventListener('keyup', function(e){
+            this.textArea.addEventListener('keypress', function(e){
                 e.preventDefault();
 
-                var countMessage = that.textArea.value.length;
-                that.textAreaTotal.innerHTML = countMessage;
-
-                that.textAreaLeft.innerHTML = that.maxChar - countMessage;
+                var charMessage = that.getChar(e);
+                var countMessage = that.textArea.value.length + 1;
+                if (charMessage && countMessage <= that.maxChar) {
+                    that.textArea.innerHTML += charMessage;
+                    that.textAreaTotal.innerHTML = countMessage;
+                    that.textAreaLeft.innerHTML = that.maxChar - countMessage;
+                }
             });
+        },
+
+        getChar: function(event) {
+            if (event.which == null) {
+                if (event.keyCode < 32) return null;
+                return String.fromCharCode(event.keyCode)
+            }
+            if (event.which != 0 && event.charCode != 0) {
+                if (event.which < 32) return null;
+                return String.fromCharCode(event.which);
+            }
+            return null;
         }
     }
 
